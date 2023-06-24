74262affc50155d3a54a54d366b914508c7677fa
Felipe Olivares <68532280+FelipeFbh@users.noreply.github.com>
2023-06-23 21:56:39 -0400

    Feat: Zerum extend LifeTime

--- media/lua/client/ZF_Zerum_Inyeccion.lua
+++ media/lua/client/ZF_Zerum_Inyeccion.lua
@@ -1,23 +1,40 @@
 function ZF_Zerum_Inyeccion(food, player, percent)
-    
+
     local bodyDamage = player:getBodyDamage(); -- retorna el body con sus stats de HP del jugador
+
+    -- si vas a debuguear coloca el siguiente valor en true
+    local debugM = false;
+    if debugM then
+        print("Inyeccion de Zerum")
+        print("Infeccion level: ")
+        print(bodyDamage:getInfectionLevel())
+        print("Infeccion time: ")
+        print(bodyDamage:getInfectionTime()) 
+        print("Infeccion Mortality Duration: ")
+        print(bodyDamage:getInfectionMortalityDuration()) 
+        print("Hours Survived: ")   
+        print(player:getHoursSurvived())
+    end
 
     local IsInfected = bodyDamage:IsInfected();
     if IsInfected == true then
-        bodyDamage:setInfectionLevel(0); -- se vuelve a setear la infeccion a 0
-        bodyDamage:setInfectionTime(bodyDamage:getHoursSurvived()); -- se vuelve a setear el tiempo de infeccion como si recien se hubiera infectado
-       
-        --notar que la infeccion en terminos de tiempo funciona tal que: 
-        -- (Tiempo_Infectado - Tiempo_Sobrevivido) = T <-Este debe llegar a T_Death para morir!
-        -- Tiempo_Estimado_A_Morir_Luego_De_Infectado = T_Death
-       
-        local bodyParts = bodyDamage:getBodyParts();
-        for i=0, bodyParts:size()-1  do --esta linea la puse pq creo q va a permitir que se les cure
-            --esa parte del cuerpo, pero sigan infectados!
-            local bodyPart = bodyParts:get(i);
-            bodyPart:SetInfected(false);
-        end
+        bodyDamage:setInfectionLevel(0.1); -- se vuelve a setear la infeccion a 0
+        bodyDamage:setInfectionTime(player:getHoursSurvived()); -- se vuelve a setear el tiempo a 0
+        bodyDamage:setInfectionMortalityDuration(player:getHoursSurvived()+20*5); -- se le da 5 dias de vida al jugador
     end
+
+    if debugM then
+        print("Inyeccion de Zerum")
+        print("Infeccion level: ")
+        print(bodyDamage:getInfectionLevel())
+        print("Infeccion time: ")
+        print(bodyDamage:getInfectionTime()) 
+        print("Infeccion Mortality Duration: ")
+        print(bodyDamage:getInfectionMortalityDuration()) 
+        print("Hours Survived: ")   
+        print(player:getHoursSurvived())
+    end
+    
 end    
 
 
